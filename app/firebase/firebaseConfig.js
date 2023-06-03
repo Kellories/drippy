4ada1925425c2c5178fe940ddb4d1f6354f8bc99
ThaiSiang <jeremytswong@gmail.com>
2023-06-03 18:24:07 +0800

    feat:fixed env

--- app/firebase/firebaseConfig.js
+++ app/firebase/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore"
 
 
 const firebaseConfig = {
-    apiKey:process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId:process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
+    apiKey:process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
   };
 let app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 let db = getFirestore(app)
